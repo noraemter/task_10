4a7b5f97a2fce58f4c839bf8bee206b866fb4c60
noraemter <111689833+noraemter@users.noreply.github.com>
2024-09-25 16:10:49 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+
+
+
+var RegForm = document.querySelector(".RegForm");
+var nameInput=document.querySelector("#name");
+var descriptionInput=document.querySelector("#description");
+var priceInput=document.querySelector("#price");
+var users=[];
+
+
+RegForm.onsubmit= function(e) {
+    e.preventDefault();  
+
+        var user = {
+            name: nameInput.value,
+            description: descriptionInput.value,
+            price: priceInput.value,
+        };
+
+       users.push(user);
+
+       console.log(users);
+       printData();
+    
+}
+
+function printData()
+{
+    var data=``;
+    for (var i = 0; i < users.length; i++) {
+        data += `
+        <tr>
+            <td>${users[i].name}</td>
+            <td>${users[i].description}</td>
+            <td>${users[i].price}</td>
+        </tr>
+        `;
+    }
+    document.querySelector("tbody").innerHTML=data;
+}
